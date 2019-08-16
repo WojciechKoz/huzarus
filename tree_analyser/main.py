2932d54bfcc221f36660119de81a3d8637e938de
Sumpree <sumpree@wp.pl>
2019-08-16 10:18:40 +0200

    aesthetic changes
    class names should be in CamelCase, inside classes one blank line between methods

--- tree_analyser/main.py
+++ tree_analyser/main.py
@@ -1,5 +1,4 @@
 from tree import PropositionalFormulaTree
-from tree import find_operator_alpha
 from analysing_tools import tree_valuation
 from smart_split import smart_split
 from converter import convert
