e540bba5979ad1d66d46936120562cf90b391839
WojciechKoz <kozlowski.wojciech092@gmail.com>
2019-08-21 10:54:58 +0200

    finished vienn diagrams for 2 and 3 sets

---
+++ sets&relations/vienn.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from vienn_managers import Double_manager, Triple_manager
+
+
+def plot_regions(manager, sets, names):
+    ''' draws vienn diagram using matplotlib libarary and given manager '''
+
+    manager.draw_circles() # draws representations of sets (big circles)
+
+    plt.xticks([]) # hides axis numbers
+    plt.yticks([])
+
+    points = manager.slots() # split sets e.g. (A, B) -> (A-B, AB, B-A)
+
+    for idx, cluster in enumerate(points):
+        # draws elements in diagram
+        plt.scatter(cluster[:, 0], cluster[:, 1], s=manager.size, c=manager.colors[idx], marker='s')
+        # prints name of each element
+        for (x, y), value in zip(cluster, manager.subsets(idx)):
+            plt.annotate(value, (x, y), ha='center', va='center', c='white')
+
+    plt.title('Vienn diagram')
+    manager.write_sets_names(names) # prints name of each set
+    plt.gca().set_aspect('equal', adjustable='box') # keeps square shape of diagram
+    # plt.axis('scaled')
+    plt.show()
+
+
+def vienn(*sets, names=None):
+    ''' runs plot_regions with some manager depends on sets amount ''' 
+    if len(sets) == 2:
+        names = ['A', 'B'] if not names else names
+        plot_regions(Double_manager(*sets), sets, names)
+    elif len(sets) == 3:
+        names = ['A', 'B', 'C'] if not names else names
+        plot_regions(Triple_manager(*sets), sets, names)
+    else:
+        raise ValueError('wrong number of sets')
--- sets&relations/vienn_managers.py
+++ sets&relations/vienn_managers.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+from matplotlib.collections import PatchCollection
 
 
-class Double_classifier:
+class Double_manager:
     ''' for vienn schema with only 2 sets, each set has to have at most 7 elements '''
     def __init__(self, set_A, set_B):
         self.A_slots  = np.array([[-0.25,0], [-0.225, 0.05], [-0.225, -0.05], \
@@ -15,7 +17,7 @@ class Double_classifier:
                 [-0.06, 0], [0.06, 0], [0, 0.04], [0, -0.04]])
 
         self.size = 240 # size of obj in plot 
-        self.colors = ('#ff0000', '#0000ff', '#990099', 'lightgreen')
+        self.colors = ('#ff0000', '#0000ff', '#990099')
 
         self.A = set_A.difference(set_B)
         self.B = set_B.difference(set_A)
@@ -23,10 +25,15 @@ class Double_classifier:
 
 
     def draw_circles(self):
-        plt.scatter([-0.1], [0], c='red', s=4*10**4, alpha=0.5)
-        plt.scatter([0.1], [0], c='blue', s=4*10**4, alpha=0.5)
-        plt.xlim(-0.4, 0.4)
+        circle_A = Circle((-0.1, 0), 0.23, color='red', alpha=0.5)
+        circle_B = Circle((0.1, 0), 0.23, color='blue', alpha=0.5)
 
+        fig, ax = plt.subplots()
+        ax.add_artist(circle_A)
+        ax.add_artist(circle_B)
+
+        plt.xlim(-0.4, 0.4)
+        plt.ylim(-0.4, 0.4)
 
 
     def slots(self):
@@ -34,8 +41,7 @@ class Double_classifier:
 
 
     def subsets(self, idx):
-        return list([self.A.difference(self.B), self.B.difference(self.A), \
-                self.A.intersection(self.B)])[idx]
+        return list([self.A, self.B, self.AB])[idx]
 
 
     def write_sets_names(self, names):
@@ -44,7 +50,7 @@ class Double_classifier:
         plt.text(0.175, 0, names[1], ha='center', va='center', fontsize=15)
 
 
-class Triple_classifier:
+class Triple_manager:
     ''' for vienn diagram with 3 sets, each set has to have at most 5 elements '''
     def __init__(self, set_A, set_B, set_C):
         self.A_slots  = np.array([[-0.14,-0.15], [-0.2, -0.1], [-0.24, -0.03], \
@@ -59,8 +65,17 @@ class Triple_classifier:
         self.AB_slots = np.array([[0,-0.09], [-0.047, -0.13], [0.047, -0.13], \
                 [-0.08, -0.08], [0.08, -0.08]])
 
+        self.AC_slots = np.array([[-0.13, 0.12], [-0.19, 0.12], [-0.16, 0.17], \
+                [-0.15, 0.06], [-0.09, 0.18]])
+
+        self.BC_slots = np.array([[0.13, 0.12], [0.19, 0.12], [0.16, 0.17], \
+                [0.15, 0.06], [0.09, 0.18]])
+
+        self.ABC_slots = np.array([[0, 0.03], [-0.05, 0], [0.05, 0], \
+                [-0.06, 0.09], [0.06, 0.09]])
+
         self.size = 240 # size of obj in plot 
-        self.colors = ('#ff0000', '#0000ff', '#999900', '#990099', 'lightgreen')
+        self.colors = ('#ff0000', '#0000ff', '#bbbb00', '#990099', '#555500', '#00ff00', '#777777')
 
         self.A = set_A.difference(set_B.union(set_C))
         self.B = set_B.difference(set_A.union(set_C))
@@ -72,23 +87,37 @@ class Triple_classifier:
 
 
     def draw_circles(self):
-        plt.scatter([-0.1], [0], c='red', s=4*10**4, alpha=0.5)
-        plt.scatter([0.1], [0], c='blue', s=4*10**4, alpha=0.5)
-        plt.scatter([0], [0.173], c='yellow', s=4*10**4, alpha=0.5)
+        circle_A = Circle((-0.1, 0), 0.23, color='red', alpha=0.5)
+        circle_B = Circle((0.1, 0), 0.23, color='blue', alpha=0.5)
+        circle_C = Circle((0, 0.173), 0.23, color='yellow',alpha=0.5)
+
+        fig, ax = plt.subplots()
+        ax.add_artist(circle_A)
+        ax.add_artist(circle_B)
+        ax.add_artist(circle_C)
+
         plt.xlim(-0.4, 0.4)
         plt.ylim(-0.3, 0.5)
 
 
     def slots(self):
         return self.A_slots[:len(self.A)], self.B_slots[:len(self.B)], self.C_slots[:len(self.C)], \
-                self.AB_slots[:len(self.AB)]
+               self.AB_slots[:len(self.AB)], self.AC_slots[:len(self.AC)], self.BC_slots[:len(self.BC)], \
+                self.ABC_slots[:len(self.ABC)]
 
 
     def subsets(self, idx):
-        return list([self.A, self.B, self.C, self.AB])[idx]
+        return list([self.A, self.B, self.C, self.AB, self.AC, self.BC, self.ABC])[idx]
 
 
     def write_sets_names(self, names):
         plt.text(-0.26, -0.09, names[0], ha='center', va='center', fontsize=15)
-        plt.text(0, -0.175, names[0]+names[1], ha='center', va='center', fontsize=15)
         plt.text(0.26, -0.09, names[1], ha='center', va='center', fontsize=15)
+        plt.text(0, 0.38, names[2], ha='center', va='center', fontsize=15)
+
+        plt.text(0, -0.175, names[0]+names[1], ha='center', va='center', fontsize=15)
+        plt.text(-0.2, 0.17, names[0]+names[2], ha='center', va='center', fontsize=15)
+        plt.text(0.2, 0.17, names[1]+names[2], ha='center', va='center', fontsize=15)
+
+        plt.text(0, 0.08, names[0]+names[1]+names[2], ha='center', va='center', fontsize=15)
+
