8bdb09ad70d43699882467c28bcbd44be38170b5
WojciechKoz <kozlowski.wojciech092@gmail.com>
2019-08-22 12:47:19 +0200

    init new branch

--- husarus/_main_truth_tab.py
+++ husarus/_main_truth_tab.py
@@ -4,7 +4,6 @@ from smart_split import smart_split
 from converter import convert
 
 
-# form = "(a or b) and (a or not c)" # if you are lazy when you have to again type new form
 form = input("enter form> ")
 B = PropositionalFormulaTree(convert(smart_split(form)))
 table = tree_valuation(B)
